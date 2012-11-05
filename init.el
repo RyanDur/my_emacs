63cff89835800704aed114efbd47792bfca2390a
RyanDurling <ryan.p.durling@gmail.com>
2012-11-04 18:47:05 -0800

    Updating my init.el

--- init.el
+++ init.el
@@ -1,14 +1,24 @@
-(menu-bar-mode  t)
-(tool-bar-mode -1)
-(ido-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Author: 
+;; Title: 
+;; From: 
+ (push "/usr/local/bin" exec-path)
+ (setq make-backup-files nil)
+ (setq auto-save-default nil)
+ (setq-default tab-width 2)
+ (setq-default indent-tabs-mode nil)
+ (setq inhibit-startup-message t)
+ (fset 'yes-or-no-p 'y-or-n-p)
+ (delete-selection-mode t)
+ (scroll-bar-mode -1)
+ (tool-bar-mode -1)
+ (blink-cursor-mode t)
+ (show-paren-mode t)
+ (column-number-mode t)
+ (set-fringe-style -1)
+ (tooltip-mode -1) 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;(add-to-list 'custom-theme-load-path "~/.emacs.dd/elpa/zenburn-1.8")
-;;(load-theme 'monokai t)
-;;(require 'zenburn)
-
-(global-linum-mode t)                    ;; show line numbers
-(column-number-mode t)                   ;; show column numbers
-(size-indication-mode t)                 ;; show file size (emacs 22+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Author: djcb
@@ -73,9 +83,84 @@
                           ("marmalade" . "http://marmalade-repo.org/packages/")))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;(load-theme 'zenburn t)
-;;(unless (zenburn-format-spec-works-p)
-;;     (zenburn-define-format-spec))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Author: Pavel Nikitin
+;; Title: Walk and ye shall reach.
+;; From: http://itstickers.blogspot.com/2010/11/all-about-emacs.html
+
+(add-to-list 'load-path "~/.emacs.d/elpa/rinari-2.9")
+(add-to-list 'load-path "~/.emacs.d/elpa/inf-ruby-2.2.3")
+(add-to-list 'load-path "~/.emacs.d/elpa/ruby-compilation-0.9")
+(add-to-list 'load-path "~/.emacs.d/elpa/jump-2.2")
+(add-to-list 'load-path "~/.emacs.d/elpa/findr-0.7")
+(add-to-list 'load-path "~/.emacs.d/elpa/inflections-1.1")
+
+;; Rinari
+(require 'rinari)
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (defadvice ruby-mode-set-encoding
+              (around ruby-mode-set-encoding-disable activate) nil)))
+
+;; Rsense
+(setq rsense-home "/usr/local/Cellar/rsense/0.3/libexec")
+(add-to-list 'load-path (concat rsense-home "/etc"))
+(require 'rsense)
+ 
+;; Rsense + Autocomplete
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (add-to-list 'ac-sources 'ac-source-rsense-method)
+            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+
+(add-to-list 'load-path "~/.emacs.d/elpa/ruby-electric-1.1")
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Author: Plillup Toland
+;; Title: dotemacs /init.el
+;; From: htps://github.com/toland/blob/master/init.el
+
+(add-to-list 'load-path "~/.emacs.d/ecb")
+(require 'ecb)
+(setq ecb-options-version "2.40"
+      ecb-tip-of-the-day nil
+      ecb-layout-name "left15"
+      ecb-show-sources-in-directories-buffer '("left7" "left13" "left14" "left15" "leftright3")
+      ecb-fix-window-size 'auto
+      ecb-windows-width 25
+      ecb-primary-secondary-mouse-buttons 'mouse-1--mouse-2
+      ecb-source-file-regexps '((".*"
+                                 ("\\(^\\(\\.\\|#\\)\\|\\(~$\\|\\.\\(elc\\|beam\\|o\\|class\\|a\\|so\\|cache\\)$\\)\\)")
+                                 ("^\\.\\(emacs\\|gnus\\)$")))
+      ecb-source-path '(("~/.emacs.d" "emacs.d")))
+(global-set-key (kbd "C-x a") 'ecb-activate)
+(global-set-key (kbd "C-x d") 'ecb-deactivate)
+(global-set-key (kbd "C-x w") 'ecb-goto-window-directories)
+(global-set-key (kbd "C-x e") 'ecb-goto-window-edit-by-smart-selection)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(menu-bar-mode  t)
+(tool-bar-mode -1)
+(ido-mode t)
+
+(add-to-list 'load-path "~/.emacs.d/elpa/sr-speedbar-0.1.8")
+(require 'sr-speedbar)
+
+(global-linum-mode t)                    ;; show line numbers
+(size-indication-mode t)                 ;; show file size (emacs 22+)
+
+(add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-1.4")
+(add-to-list 'load-path "~/.emacs.d/elpa/popup-0.5")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
+(ac-config-default)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -83,8 +168,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes (quote (monokai)))
- '(custom-safe-themes (quote ("71efabb175ea1cf5c9768f10dad62bb2606f41d110152f4ace675325d28df8bd" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default))))
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-safe-themes (quote ("71efabb175ea1cf5c9768f10dad62bb2606f41d110152f4ace675325d28df8bd" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+ '(ecb-options-version "2.40")
+ '(ecb-source-file-regexps (quote ((".*" ("\\(^\\(\\.\\|#\\)\\|\\(~$\\|\\.\\(elc\\|obj\\|o\\|class\\|lib\\|dll\\|a\\|so\\|cache\\)$\\)\\)") ("^\\.\\(emacs\\|gnus\\)$"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
